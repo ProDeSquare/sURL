476b513e89cc0a92489c902c1819108672d4f515
Hamza Mughal <hamza@prodesquare.com>
2022-01-31 00:37:34 +0500

    saves table

---
+++ database/migrations/2022_01_30_193437_create_saves_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSavesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('saves', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->string('title');
+            $table->text('url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('saves');
+    }
+}
