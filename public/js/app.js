dfbc0ccc7b3c3acf73068f834b81a780fed0566b
Hamza Mughal <hamza@prodesquare.com>
2023-01-28 03:30:18 +0500

    Short view page skeleton ready

--- public/js/app.js
+++ public/js/app.js
@@ -21643,10 +21643,18 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
 /* harmony import */ var _Layouts_AppLayout__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @/Layouts/AppLayout */ "./resources/js/Layouts/AppLayout.vue");
+/* harmony import */ var _Jetstream_SectionBorder__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @/Jetstream/SectionBorder */ "./resources/js/Jetstream/SectionBorder.vue");
+
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
+  props: {
+    "short": Object,
+    collection: Object,
+    clicks: Object
+  },
   components: {
-    AppLayout: _Layouts_AppLayout__WEBPACK_IMPORTED_MODULE_0__["default"]
+    AppLayout: _Layouts_AppLayout__WEBPACK_IMPORTED_MODULE_0__["default"],
+    JetSectionBorder: _Jetstream_SectionBorder__WEBPACK_IMPORTED_MODULE_1__["default"]
   }
 });
 
@@ -28027,32 +28035,85 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.esm-bundler.js");
 
+var _hoisted_1 = {
+  "class": "font-semibold text-3xl text-gray-800 leading-tight tracking-wide truncate"
+};
+var _hoisted_2 = {
+  "class": "py-12"
+};
+var _hoisted_3 = {
+  "class": "max-w-7xl mx-auto sm:px-6 lg:px-8"
+};
+var _hoisted_4 = {
+  "class": "text-gray-700"
+};
+var _hoisted_5 = {
+  "class": "truncate"
+};
 
-var _hoisted_1 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("h2", {
-  "class": "font-semibold text-3xl text-gray-800 leading-tight tracking-wide"
-}, " Short title ", -1
+var _hoisted_6 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("span", {
+  "class": "font-semibold"
+}, "URL: ", -1
 /* HOISTED */
 );
 
-var _hoisted_2 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", {
-  "class": "py-12"
-}, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", {
-  "class": "max-w-7xl mx-auto sm:px-6 lg:px-8"
-}, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", {
-  "class": "text-sm text-gray-700"
-}, "Short Details...")])], -1
+var _hoisted_7 = ["href"];
+var _hoisted_8 = {
+  "class": "truncate"
+};
+
+var _hoisted_9 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("span", {
+  "class": "font-semibold"
+}, "sURL: ", -1
+/* HOISTED */
+);
+
+var _hoisted_10 = ["href"];
+var _hoisted_11 = {
+  "class": "text-gray-600"
+};
+
+var _hoisted_12 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("span", {
+  "class": "font-semibold text-gray-700"
+}, "Created: ", -1
+/* HOISTED */
+);
+
+var _hoisted_13 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("span", {
+  "class": "font-semibold text-gray-700"
+}, "Last Updated: ", -1
 /* HOISTED */
 );
 
 function render(_ctx, _cache, $props, $setup, $data, $options) {
+  var _component_jet_section_border = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("jet-section-border");
+
   var _component_app_layout = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("app-layout");
 
   return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createBlock)(_component_app_layout, null, {
     header: (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
-      return [_hoisted_1];
+      return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("h2", _hoisted_1, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)($props["short"].title), 1
+      /* TEXT */
+      )];
     }),
     "default": (0,vue__WEBPACK_IMPORTED_MODULE_0__.withCtx)(function () {
-      return [_hoisted_2];
+      return [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_2, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_3, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_4, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_5, [_hoisted_6, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("a", {
+        "class": "text-purple-600 hover:text-purple-800",
+        href: $props["short"].url
+      }, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)($props["short"].url), 9
+      /* TEXT, PROPS */
+      , _hoisted_7)]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_8, [_hoisted_9, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("a", {
+        "class": "text-purple-600 hover:text-purple-800",
+        href: _ctx.route('short-redirect', $props["short"])
+      }, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(_ctx.route('short-redirect', $props["short"])), 9
+      /* TEXT, PROPS */
+      , _hoisted_10)]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_11, [_hoisted_12, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createTextVNode)(" " + (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)($props["short"].created_at) + " - ", 1
+      /* TEXT */
+      ), _hoisted_13, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createTextVNode)(" " + (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)($props["short"].updated_at), 1
+      /* TEXT */
+      )])]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_jet_section_border), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", null, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)($props.clicks), 1
+      /* TEXT */
+      )])])];
     }),
     _: 1
     /* STABLE */
--- resources/js/Pages/Short/ViewShort.vue
+++ resources/js/Pages/Short/ViewShort.vue
@@ -1,25 +1,53 @@
 <template>
     <app-layout>
         <template #header>
-            <h2 class="font-semibold text-3xl text-gray-800 leading-tight tracking-wide">
-                Short title
+            <h2 class="font-semibold text-3xl text-gray-800 leading-tight tracking-wide truncate">
+                {{ short.title }}
             </h2>
         </template>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <p class="text-sm text-gray-700">Short Details...</p>
+                <div class="text-gray-700">
+                    <div class="truncate">
+                        <span class="font-semibold">URL: </span>
+                        <a class="text-purple-600 hover:text-purple-800" :href="short.url">{{ short.url }}</a>
+                    </div>
+                    <div class="truncate">
+                        <span class="font-semibold">sURL: </span>
+                        <a class="text-purple-600 hover:text-purple-800" :href="route('short-redirect', short)">{{ route('short-redirect', short) }}</a>
+                    </div>
+                    <div class="text-gray-600">
+                        <span class="font-semibold text-gray-700">Created: </span>
+                        {{ short.created_at }} -
+                        <span class="font-semibold text-gray-700">Last Updated: </span>
+                        {{ short.updated_at }}
+                    </div>
+                </div>
+
+                <jet-section-border></jet-section-border>
+
+                <div>
+                    {{ clicks }}
+                </div>
             </div>
         </div>
     </app-layout>
 </template>
 
 <script>
-import AppLayout from '@/Layouts/AppLayout'
+    import AppLayout from '@/Layouts/AppLayout'
+    import JetSectionBorder from '@/Jetstream/SectionBorder'
 
-export default {
-    components: {
-        AppLayout,
+    export default {
+        props: {
+            short: Object,
+            collection: Object,
+            clicks: Object,
+        },
+        components: {
+            AppLayout,
+            JetSectionBorder,
+        }
     }
-}
 </script>
